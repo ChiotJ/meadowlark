cc99d09da0c903cda81ca1d8e5e8f46cb541e722
unknown <>
2015-12-09 22:04:44 +0800

    Initial commit

---
+++ site/meadowlark.js
@@ -0,0 +1,50 @@
+/**
+ * Created by jian_ on 2015/12/8.
+ */
+var express = require('express');
+var app = express();
+
+// 设置 handlebars 视图引擎
+var handlebars = require('express3-handlebars')
+    .create({defaultLayout: 'main.hbs'});
+app.engine('hbs', handlebars.engine);
+app.set('view engine', 'hbs');
+
+app.set('port', process.env.PORT || 3000);
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.get('/about', function (req, res) {
+    var fortunes = [
+        "Conquer your fears or they will conquer you.",
+        "Rivers need springs.",
+        "Do not fear what you don't know.",
+        "You will have a pleasant surprise.",
+        "Whenever possible, keep it simple."
+    ];
+    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', {fortune: randomFortune});
+});
+
+// 定制 404 页面
+app.use(function (req, res) {
+    res.status(404);
+    res.render('404');
+});
+
+// 定制 500 页面
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
